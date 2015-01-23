ee54964a25789aeb7b0c9e0e93744dec1add2b62
Terry Yin <terry@odd-e.com>
2015-01-23 10:24:43 +0800

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Group A</title>
+        <link href="group_a.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <h1>This is group a Pte Ltd</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
